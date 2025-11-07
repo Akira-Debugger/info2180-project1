02ee5e964888ce4e7045c34f0734ac6a7ee326dc
Jevaughn Johnson <johnjack4135@gmail.com>
2025-11-07 04:53:05 -0500

    Implementation of javascript and little CSS change

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+    let form = document.querySelector('form');
+    let display_email=document.querySelector('.message');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        let forminput=document.getElementById('email').value;
+        if(forminput =='') {
+         display_email.textContent = "Please enter a valid email address.";
+
+        }else {
+            display_email.textContent = "Thank you for subscribing to our newsletter, " + forminput + "!";
+
+        }
+
+    });
+
+});
